8d02a0ccb1bbc53258bc7377a92a0f4306576f70
disha123-bm <118367662+disha123-bm@users.noreply.github.com>
2023-03-28 13:59:58 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,16 @@
+import argparse
+
 def reverse_string(string):
     reversed_string = ''
     for i in range(len(string)-1, -1, -1):
         reversed_string += string[i]
     return reversed_string
 
-string = input("Enter a string: ")
-reversed_string = reverse_string(string)
+parser = argparse.ArgumentParser(description='Reverse a string')
+parser.add_argument('input_string', type=str, help='The string to reverse')
+
+args = parser.parse_args()
+input_string = args.input_string
+
+reversed_string = reverse_string(input_string)
 print("Reversed string:", reversed_string)
